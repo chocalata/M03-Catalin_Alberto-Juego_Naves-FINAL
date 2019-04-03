666343c8725bc06f3353cf8f1d6cb6fc5da8327e
Alberto <agutierroj99@elpuig.xeill.net>
2019-04-03 16:35:00 +0200

    Menu Principal

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        primaryStage.setTitle("APOLO X");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
 
